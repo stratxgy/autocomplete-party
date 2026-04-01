a45c8a5954b112a0b27b84e86a3d697f3321d0dd
stratxgy <117533771+stratxgy@users.noreply.github.com>
2026-03-31 21:07:08 -0500

    Update host.js

--- js/host.js
+++ js/host.js
@@ -417,5 +417,5 @@ function escHtml(s) {
   return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
 }
 
-// ─── INIT ─────────────────────────────────────────────────────────────────────
+
 initHost();
