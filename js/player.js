5987df113e30336e0d8828f5583c546f71ff8c97
stratxgy <117533771+stratxgy@users.noreply.github.com>
2026-03-31 19:21:35 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -57,7 +57,30 @@ function joinGame() {
   errEl.textContent = '';
   showScreen('screen-connecting');
 
-  peer = new Peer({ debug: 0 });
+  peer = new Peer({
+    debug: 0,
+    config: {
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        {
+          urls: 'turn:openrelay.metered.ca:80',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        }
+      ]
+    }
+  });
 
   peer.on('open', () => {
     hostConn = peer.connect(resolvedHostId, { reliable: true });
